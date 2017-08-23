10be83228ee36d1cc27fd64f344b87a91ce9b154
Matt Snow <matthew.j.snow@gmail.com>
2017-08-23 09:29:45 -0600

    slots working

--- casino.rb
+++ casino.rb
@@ -1,9 +1,59 @@
+# require 'pry'
+# require_relative 'player'
+# require_relative 'high_low'
+# require_relative 'wallet'
+# require_relative 'roulette'
+# require_relative 'blackjack'
+# class Casino
+#   attr_accessor :players
+#
+#   def initialize
+#     puts 'Welcome To The Ruby Casino'
+#     @player = Player.new
+#     menu
+#   end
+#
+#   def menu
+#     exit if out_of_money
+#     puts "1) High Low"
+#     puts "2) Roulette"
+#     puts "3) Slots"
+#     puts "4) Blackjack"
+#     puts "5) Quit"
+#     case gets.to_i
+#     when 1
+#       HighLow.new(@player)
+#     when 2
+#       Roulette.new(@player)
+#     when 3
+#       Slots.new(@player)
+#     when 4
+#       Blackjack.new(@player)
+#     when 5
+#       puts 'Thanks for playing'
+#       exit
+#     else
+#       puts 'Invalid Choice! Try Again'
+#       menu
+#     end
+#     menu
+#   end
+#   def out_of_money
+#     @player.wallet.amount == 0
+#   end
+# end
+#
+# Casino.new
+
+
 require 'pry'
 require_relative 'player'
 require_relative 'high_low'
 require_relative 'wallet'
 require_relative 'roulette'
+require_relative 'slots'
 require_relative 'blackjack'
+
 class Casino
   attr_accessor :players
 
@@ -26,7 +76,7 @@ class Casino
     when 2
       Roulette.new(@player)
     when 3
-      Slots.new(@player)
+      Slots.new(@player, self)
     when 4
       Blackjack.new(@player)
     when 5
--- slots.rb
+++ slots.rb
@@ -1,63 +1,81 @@
 require_relative 'player'
 require_relative 'wallet'
+require 'pry'
 
 class Slots
-  def initialize(player)
-    first_slot = ["BAR","$$$",""]
-    second_slot = ["BAR","$$$",""]
-    third_slot = ["BAR","$$$",""]
-    first_slot.sample = slot_1
-    second_slot.sample = slot_2
-    thrid_slot.sample = slot_3
-    if slot_1 == slot_2 && slot_2 == slot_3
-      player.wallet.amount = player.wallet.amount
+  attr_accessor :player, :casino
+  def initialize(player, casino)
+    @casino = casino
+@player=player
+  insert_money
 
-
-    puts "Welcome to Slots #{player.name}"
-    puts "Rules: Place your bet of either $1, $2, or $5, then spin the wheel!!!"
-    puts "If you match 3 numbers, YOU WIN!!!"
-    puts "If you match 2 numbers, SPIN AGAIN!!!"
-    puts "if you don't match any numbers, YOU LOSE"
-    puts "$1 odds are 50:1"
-    puts "$2 odds are 100:1"
-    puts "$5 odds are 500:1"
-    puts
-    puts "1: Play"
-    puts "2: Leave spot at the Slots"
-    input = gets.strip
-      if  input == '1' && player.wallet.amount >= 1
-        bet(player)
-      elsif
-        puts "Thanks for playing but you're out of money!!!"
-        puts "Have a nice day!!!"
-        exit
-      else
-      end
   end
-
+def insert_money
+  puts "Welcome to Slots #{@player.name}"
+  puts "Rules: Place your bet then spin the wheel!!!"
+  puts "If you match 3 numbers, YOU WIN!!!"
+  puts "if you don't match any numbers, YOU LOSE"
+  puts
+  puts "1: Play"
+  puts "2: Leave spot at the Slots"
+  input = gets.strip
+    if  input == '1' && @player.wallet.amount >= 1
+      bet(@player)
+    elsif input =='2'
+      puts "Have a nice day!!!"
+      Casino
+    elsif input != ['1'..'2']
+      insert_money
+    else input == '1' && @player.wallet.amount == 0
+      puts "Sorry but you are broke, come back when you have more money!"
+      exit
+    end
+  end
   def bet(player)
-    #puts "You have \$#{player.wallet.amount} to bet with!"
+    puts "You have $#{@player.wallet.amount} to bet with!"
     puts "How much do you want to bet?"
     print "$"
-    bet = gets.to_f
-    if bet > player.wallet.amount
+    wager = gets.to_i
+    if wager > player.wallet.amount
      puts "Thats more than you have."
-     bet(player)
-   elsif bet < 1
+     bet(@players)
+   elsif wager < 1
       puts "Must bet at least $1"
-      bet(player)
+      bet(@player)
     else
-      play(player, bet)
+      play(player, wager)
     end
+  end
+  def roll
+    @probability = ['cherry', 'cherry', 'cherry', 'bar', 'bar', 'bar', '7', '7', '7']
+    spin_results = []
+    spin_results << @probability.sample
+    spin_results << @probability.sample
+    spin_results << @probability.sample
   end
 
   def play(player, bet)
     puts "Press Enter to SPIN!!!"
-    @number.sample
+    spin_results = roll
+    if spin_results == [ 'cherry', 'cherry', 'cherry' ]
+      puts 'WINNER!!!'
+      player.wallet.add(bet * 5)
+      insert_money
+    elsif
+      spin_results == [ 'BAR', 'BAR', 'BAR' ]
+      puts 'WINNER!!!'
+      player.wallet.add(bet * 10)
+    elsif
+      spin_results == [ '7', '7', '7' ]
+      puts 'WINNER!!!'
+      player.wallet.add(bet * 77)
     else
+      puts
+      puts "#{spin_results[0]} - #{spin_results[1]} - #{spin_results[2]}"
       puts "You Lose."
       player.wallet.subtract(bet)
+      insert_money
     end
-    initialize(player)
+    insert_money
   end
 end
